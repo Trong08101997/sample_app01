fbbf05ba431052c3313b6229122f8d259694ada2
Trong08101997 <nguyentrong08101997@gmail.com>
2018-01-09 18:12:39 +0700

    Finish layout and routes

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,10 +1,9 @@
 module StaticPagesHelper
-  def full_title(page_title = '')                     # Method def, optional arg
-    base_title = "Ruby on Rails Tutorial Sample App"  # Variable assignment
-    if page_title.empty?                              # Boolean test
-      base_title                                      # Implicit return
-    else
-      page_title + " | " + base_title                 # String concatenation
+  def full_title(page_title = "")                     
+    base_title = "Ruby on Rails Tutorial Sample App"  
+    if page_title.empty?                              
+      base_title                                     
+      page_title + " | " + base_title     
     end
   end
 end
